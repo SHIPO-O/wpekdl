3ecd0e0cd9b737beb790b7d7298d43c7dc11d218
BM6100 <hwanbum4056@gmail.com>
2025-06-12 21:30:46 +0900

    spring project

---
+++ src/main/java/org/springframework/boot/gedai/GedaiApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.gedai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GedaiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/springframework/boot/gedai/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.gedai;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GedaiApplication.class);
+	}
+
+}
